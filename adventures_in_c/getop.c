0db87b32f5346be722fd6c11aab2eee423fc4d6f
Nic M <nicdevel@gmail.com>
2013-11-06 22:43:43 -0500

    short_options
    initial implementation to demonstrate use of getopt. Couple of gotchas with constant arrays and also static modifier on main. Will clean up and keep the exercise going

--- adventures_in_c/getop.c
+++ adventures_in_c/getop.c
@@ -1,7 +1,62 @@
-/* -*- mode: c; fill-column: 72; coding: utf-8-unix; -*- */
+/* -*- mode: c; ; fill-column: 80; coding: utf-8-unix; -*- */
 /*
   Nic M
   Nov 2013
-
   
+  Adventures in C: Commandline Arguments
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 		/* getopt */
+#include <getopt.h> 		/* getopt_long, getopt_long_only */
+
+static const unsigned short opt_flag; /* useful in option struct declaration */
+
+static int short_options(const int *argc, char ** argv){
+  /* Process Short options only with getop and return optind*/
+  unsigned short aflag,bflag,cflag = 0;
+  char *cvalue,*dvalue = NULL;
+
+  int opt; 
+  while((opt = getopt(*argc,argv,"abc:d:")) != 0){
+    switch(opt){
+    case 'a':
+      aflag = 1; 
+      break;
+    case 'b':
+      bflag = 1; 
+      break; 
+    case 'c':
+      cvalue = optarg; 
+      break;
+    case 'd': 
+      dvalue = optarg; 
+      break;
+    case '?': 			/* failed to process arg */
+      if (optopt == 'c'||optopt == 'd')
+	fprintf(stderr, "Option -%c requires an argument\n",optopt);
+      else if (isprint (optopt))
+	fprintf(stderr,"Unknown option -%c\n",optopt);
+      else 
+	fprintf(stderr,"Unkown option character `\\x%x .\n", optopt);
+      return -1;
+    default:
+      abort();
+    }
+  }
+  return optind; 		/* last position in argv */
+}
+
+int main(const int argc, char **argv){
+  /*
+    main is how the outside world gets into the application/this file
+    and cannot therefore be decrared as static!
+  */
+  int post_optc;
+  if (argc > 0)
+    post_optc = short_options(&argc,argv);
+
+  return 0;
+}
+
